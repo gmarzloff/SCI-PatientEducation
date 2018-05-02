34b6eb588f92143ad590f36f44a9b20015fd92e3
George Marzloff <george@marzloffmedia.com>
2018-05-02 11:55:35 -0400

    added stackPages:true property in app to allow for multi-level back navigation

--- js/app.js
+++ js/app.js
@@ -8,8 +8,10 @@ var app = new Framework7({
     name: 'SCI Patient Education',
     id: 'com.openrehab.scipteducation',
     theme: 'ios', // can be 'ios', 'md' for android, or 'auto' which picks md for everything but iOS devices
-    routes: routes  // defined in routes.js
+    routes: routes,  // defined in routes.js,
+    view: {
+        stackPages: true /* saves more than 2 pages in the DOM so user can still return to the index.html page after following multiple links */
+    }
 });
 
-var mainVew = app.views.create('.view-main');
-
+var mainView = app.views.create('.view-main');
