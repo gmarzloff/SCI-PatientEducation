bda46af6181b94d020f8e77af879f38303a3fbf3
Marwa <marwa0825@yahoo.com>
2018-05-02 21:57:26 -0500

    table voila

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ var app = new Framework7({
     id: 'com.openrehab.scipteducation',
     theme: 'ios', // can be 'ios', 'md' for android, or 'auto' which picks md for everything but iOS devices
     routes: routes,  // defined in routes.js,
+    xhrCache: false, 
     view: {
         stackPages: true /* saves more than 2 pages in the DOM so user can still return to the index.html page after following multiple links */
     }
